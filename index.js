22a68bb7e4193aed41619daf9c67d99d6dfe3ec5
Caroline <bancroftwanjiru10000@gmail.com>
2024-07-02 11:08:07 +0300

    pending

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+
+const employee = {
+   name:"caroline", streetAddresse:"Mukuyu Road"}
+
+
+//defined and assigned a function that updates the employee with a key and a value (non-destructively)
+function updateEmployeeWithKeyAndValue(employee, key, value){
+  return {...employee,[key]:value};
+}
+//defined and assigned a function that updates the employee with a key and a value (destructively)
+function  destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+  employee [key] = value
+  return employee
+}
+
+// delete key from the call of the employee 
+function nonDestructivedeleteEmployeeByKey(employee, key){
+ 
+   delete {...employee,[key]:value}
+   return employee
+  
+  
+
+}
+
+
+
---
+++ rough.js
@@ -0,0 +1,9 @@
+function employee (name, streetAddress){
+}
+
+
+console.log(employee("caroline", "Mukuyu Road"))
+
+const anotherEmployee = new employee("lennah", "streetAddress")
+console.log(anotherEmployee.name = "lennah")
+console.log(anotherEmployee)
